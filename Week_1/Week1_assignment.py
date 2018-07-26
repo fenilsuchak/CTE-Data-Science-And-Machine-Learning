b25a183ccb6c880a97a96410374fd919831c0809
Pranav Mahajan <mahajan.pranav25@gmail.com>
2018-07-26 12:06:01 +0530

    Additions to Part3

--- Week_1/Week1_assignment.py
+++ Week_1/Week1_assignment.py
@@ -131,3 +131,156 @@ def customer_login_func(*args, **kwargs):
     
 customer_prime_sum = customer_login_func() #Add arguments if any.
     
+
+
+
+#Part 3-Basic Matrix manipulations
+"""
+NumPy is one of the best ways to manipulate and do matrix operations in python.
+
+The following code will take you through the basics of NumPy
+
+"""
+
+import numpy as np#importing NumPy module with name as np
+
+#Initialising an array :
+
+#creating an 1d array:
+array1=np.array([2,3])
+print(array1)
+
+#creating an 2-d array:
+array2=np.array([[3,8],[5,6],[7,2]])
+print(array2)
+
+#getting the shape of an array
+print(array2.shape)
+
+#arange returns evenly spaced values within a given interval.
+array3 = np.arange(0, 15, 2) # start at 0 count up by 2, stop before 15
+print(array3)
+
+#reshaping an array
+#reshape returns an array with the same data with a new shape.
+array4=array2.reshape(2,3)
+print(array4)
+
+#resize changes the shape and size of array in-place.
+array2.resize(2, 3) #no_need to do array2 = array2.reshape(2,3)
+print(array2)
+
+#linspace returns evenly spaced numbers over a specified interval
+array5= np.linspace(0, 4, 9) # return 9 evenly spaced values from 0 to 4
+print(array5)
+
+#ones returns a new array of given shape and type, filled with ones.
+print(np.ones((3, 2)))
+
+#zeros returns a new array of given shape and type, filled with zeros.
+print(np.zeros((2, 3)))
+
+#eye returns a 2-D array with ones on the diagonal and zeros elsewhere.
+print(np.eye(3))
+
+#diag extracts a diagonal or constructs a diagonal array.
+array5=np.array([[3,4,5],[5,6,4],[45,23,43]])
+print(array5)
+print(" the diagonal array: ",np.diag(array5))
+
+#Create an array using repeating list (or see np.tile)
+array6=np.array([1, 2, 3] * 3)
+print(array6)
+
+#Repeat elements of an array using repeat.
+array7=np.repeat([1, 2, 3], 3)
+print(array7)
+
+#Slicing rows and coloumns 
+array8 = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+"""
+  Two ways of accessing the data in the middle row of the array.
+  Mixing integer indexing with slices yields an array of lower rank,
+  while using only slices yields an array of the same rank as the 
+"""
+# original array:
+row_r1 = array8[1, :]    # Rank 1 view of the second row of a
+row_r2 = array8[1:2, :]  # Rank 2 view of the second row of a
+print(row_r1, row_r1.shape)  # Prints "[5 6 7 8] (4,)"
+print(row_r2, row_r2.shape)  # Prints "[[5 6 7 8]] (1, 4)"
+
+# We can make the same distinction when accessing columns of an array:
+col_r1 = array8[:, 1]
+col_r2 = array8[:, 1:2]
+print(col_r1, col_r1.shape)  # Prints "[ 2  6 10] (3,)"
+print(col_r2, col_r2.shape)  # Prints "[[ 2]
+                             #          [ 6]
+                             #          [10]] (3, 1)"
+
+#Datatypes
+array9 = np.array([5, 10])   # Let numpy choose the datatype
+print(array9.dtype)          # Prints "int64"
+
+array9 = np.array([5.0, 10.0])   # Let numpy choose the datatype
+print(array9.dtype)              # Prints "float64"
+
+array9 = np.array([5, 10], dtype=np.float64)   # Force a particular datatype
+print(array9.dtype)                         # Prints "float64"
+
+#Transpose
+array10 = np.array([[1,2], [3,4]])
+print(array10)    # Prints "[[1 2]
+            	  #          [3 4]]"
+print(array10.T)  # Prints "[[1 3]
+                  #          [2 4]]"
+
+# Note that taking the transpose of a rank 1 array does nothing:
+vector1 = np.array([1,2,3])
+print(vector1)    # Prints "[1 2 3]"
+print(vector1.T)  # Prints "[1 2 3]"
+
+"""
+NOTE: When you initialise 2 arrays and add(+),subtract(-), multiply(*), divide(/),
+Operations would be ELEMENT-wise, array1*array2 won't give you matrix multiplication unlike in MATLAB.
+Experiment with different datatypes and arraysizes while carrying out these operation show which one leads to errors.
+TRY IT OUT below with x,y,v, or any other array you want.
+"""
+
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = np.empty_like(x)   # Create an empty matrix with the same shape as x
+
+y = x + v  
+print(y)
+#What do you notice? How can we add 2 arrays of different sizes? What is happening?
+"""
+You just added v to each row of x using broadcasting.
+The line y = x + v works even though x has shape (4, 3) and v has shape (3,) due to broadcasting;
+this line works as if v actually had shape (4, 3), where each row was a copy of v,
+and the sum was performed elementwise.
+
+While working with array6, have you checked out np.tile? If not check it out.
+"""
+vv = np.tile(v, (4, 1))   # Stack 4 copies of v on top of each other
+print(vv)                 # Prints "[[1 0 1]
+                          #          [1 0 1]
+                          #          [1 0 1]
+                          #          [1 0 1]]"
+y = x + vv  # Add x and vv elementwise
+print(y)  # Prints "[[ 2  2  4
+          #          [ 5  5  7]
+          #          [ 8  8 10]
+          #          [11 11 13]]"
+
+#This does the same job as above or else you could use a loop as well, like shown below
+for i in range(4):
+    y[i, :] = x[i, :] + v
+
+# Now y is the following
+# [[ 2  2  4]
+#  [ 5  5  7]
+#  [ 8  8 10]
+#  [11 11 13]]
+print(y)
+
+#GOOD JOB!! CHECKOUT NUMPY DOCUMENTATION FOR MORE!!
